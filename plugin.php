766fc9f558dfbdf9d3b83ada04dc26aa68af737c
mardojai <mardojai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-26 03:12:13 +0000

    tag version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/simple-alert-boxes/tags/1.2@1230906 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,71 @@
+<?php 
+/**
+ * Plugin Name: Simple Alert Boxes
+ * Plugin URI: http://www.rafael.mardojai.com/simple-alert-boxes-plugin
+ * Description: Use responsives alert boxes with shortcodes.
+ * Version: 1.2
+ * Author: Rafael Mardojai C.M.
+ * Author URI: http://www.rafael.mardojai.com
+  * License: A short license name. Example: GPL2
+ */
+
+/*  Copyright 2015 Rafael Mardojai C.M.  (email : mardojai.cardenas@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+/** Head Enqueue **/
+function simple_alert_boxes_scripts() {
+	wp_enqueue_style( 'simple-alert-boxes', plugins_url( 'css/simple-alert-boxes.css', __FILE__ )  );	
+    wp_enqueue_style( 'simple-alert-boxes-icons', plugins_url( 'css/font-awesome.min.css', __FILE__ )  );	
+}
+add_action( 'wp_enqueue_scripts', 'simple_alert_boxes_scripts' );
+?>
+
+<?php
+/** ShortCode **/
+add_shortcode( 'alert', 'alert_output' );
+function alert_output( $atts, $content ) {
+    $atts = shortcode_atts( array(
+        'type' => '',
+        'text' => ''
+    ), $atts );
+    return '<div class="alert ' . $atts['type'] . '"><p>' . $atts['text'] . '' . $content . '</p></div>';
+}
+?>
+
+<?php
+/** TinyMCE Plugin **/
+function alert_boxes_tinymce() {
+    global $typenow;
+
+    if( ! in_array( $typenow, array( 'post', 'page' ) ) )
+        return ;
+
+    add_filter( 'mce_external_plugins', 'fb_add_tinymce_plugin' );    
+    add_filter( 'mce_buttons', 'fb_add_tinymce_button' );
+}
+add_action( 'admin_head', 'alert_boxes_tinymce' );
+
+function fb_add_tinymce_plugin( $plugin_array ) {
+    $plugin_array['simple_alert_boxes'] = plugins_url( '/js/plugin.js', __FILE__ );    
+    return $plugin_array;
+}
+
+function fb_add_tinymce_button( $buttons ) {
+    array_push( $buttons, 'alert_boxes_button_key' );    
+    return $buttons;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Simple Alert Boxes ===
+Contributors: mardojai
+Tags: shortcodes, alert, boxes, mensajes, alertas, mensagges
+Requires at least: 3.0.1
+Tested up to: 4.3.0
+Stable tag: 4.3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Use responsives alert boxes with shortcodes.
+
+== Description ==
+
+Use responsives alert boxes with shortcodes.
+
+Is easy, only use the shortcodes.
+
+Example: For a success mensagge use: [alert type="success"]Your text here[/alert], for a info mensagge: [alert type="info"]Your text here[/alert], for a warning mensagge: [alert type="warning"]Your text here[/alert] and for a danger mensagge: [alert type="danger"]Your text here[/alert].
+
+* DEMO HERE: http://www.rafael.mardojai.com/simple-alert-boxes-plugin/
+* GITHUB REPOSITORY: https://github.com/mardojai/Simple-Alert-Boxes-Wordpress-Plugin
+
+
+== Installation ==
+
+1. Download the plugin
+2. Upload the plugin directory to the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. screenshot-1.png
+1. screenshot-2.png
+
+== Changelog ==
+
+= 1.2 =
+* TinyMCE Plugin
+
+= 1.1 =
+* Optimized Shortcodes
+* Support bold, italic, links and more
+
+= 1.0 =
+* Responsive Design
+* Shortcodes
